efc16ba33b9ae59ec5ef4c4b06985a684fdd697e
梁波 <13883386902@139.com>
2020-01-23 09:48:15 +0800

    add

---
+++ src/main/java/com/lglq/ms/bs/gw/GwApp.java
@@ -0,0 +1,12 @@
+package com.lglq.ms.bs.gw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+
+@SpringCloudApplication
+public class GwApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GwApp.class, args);
+	}
+}
